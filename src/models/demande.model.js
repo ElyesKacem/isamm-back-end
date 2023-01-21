5c5604f0dcc606c0ceaf5e761d01f2551e9ca2cc
MhamedSanaa <mhamed.sanaa6@gmail.com>
2023-01-21 23:04:17 +0100

    tweaking

--- src/models/demande.model.js
+++ src/models/demande.model.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const demandeSchema = new Schema({
+  student_id:{
+    type: Schema.Types.ObjectId,
+    ref: 'student'
+  },
   title: {
     type: String,
     required: true,
